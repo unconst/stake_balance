6747b9f6a5cf3d59a5047f9572c3e4bcdba44fc2
unconst <jake@bittensor.com>
2023-06-02 20:51:45 -0700

    next

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,10 +14,11 @@
 
 import streamlit as st
 import bittensor as bt
+bt.trace()
 
 hotkey_mnemonic = st.text_input('Enter your hotkey mnemonic', '')
-uid_to_query = int( st.text_input('Enter a UID', '') )
-message = str( st.text_input('Enter a Message', '') )
+uid_to_query = st.text_input('Enter a UID', '')
+message = st.text_input('Enter a Message', '')
 
 wallet = bt.wallet( name = 'streamlit', hotkey = 'streamlit')
 wallet.regenerate_hotkey( mnemonic = hotkey_mnemonic, overwrite = True, use_password = False)
@@ -26,7 +27,7 @@ st.write( 'UID to query', uid_to_query )
 
 try:
     meta = bt.metagraph( 1 )
-    dend = bt.text_prompting( keypair = wallet.hotkey, axon = meta.axons[ uid_to_query ] )
+    dend = bt.text_prompting( keypair = wallet.hotkey, axon = meta.axons[ int( uid_to_query ) ] )
     resp = dend.forward( roles = ['user'], message = [ message ] )
     st.write(f'Completion: \t{ resp.completion }')
 except:
