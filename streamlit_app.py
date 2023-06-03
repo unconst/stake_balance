10a13d3eb3d94942628656fbd4dee631bfcd295d
unconst <jake@bittensor.com>
2023-06-02 19:28:53 -0700

    change reqs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import bittensor as bt
 
-coldkey_address = st.text_input('Your coldkey ss58 address', '0x')
+coldkey_address = st.text_input('Your coldkey ss58 address', '')
 st.write( 'Your coldkey ss58 address is:', coldkey_address )
 
 try:
     sub = bt.subtensor()
     delegates = sub.get_delegated( coldkey_ss58 = coldkey_address )
-    st.write(f'Delegate: {str(delegates[0][0].hotkey_ss58)} amount { delegates[0][1]}' )
+    st.write(f'Delegate: {str(delegates[0][0].hotkey_ss58)} \nAmount: { delegates[0][1]}' )
 except:
     pass
 
