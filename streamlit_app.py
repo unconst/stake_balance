84b8e905cd36d8ef8b3c714d2c553f3a59c3931e
unconst <jake@bittensor.com>
2023-06-02 19:18:54 -0700

    next

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import bittensor as bt
 
 coldkey_address = st.text_input('Your coldkey ss58 address', '0x')
 st.write( 'Your coldkey ss58 address is:', coldkey_address )
+
+sub = bt.subtensor()
+delegates = sub.get_delegated( coldkey_ss58 = coldkey_address )
+st.write( 'Delegates', str(coldkey_address) )
+
